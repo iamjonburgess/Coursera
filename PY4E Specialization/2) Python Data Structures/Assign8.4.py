44f3b3deeb98b77cd843eb156468e1f5c6454aa8
Jon <56128717+iamjonburgess@users.noreply.github.com>
2021-12-18 13:42:03 -0500

    Create Assign8.4.py

---
+++ PY4E Specialization/2) Python Data Structures/Assign8.4.py
@@ -0,0 +1,32 @@
+"""
+8.4 Open the file romeo.txt and read it line by line. For each line, split the
+line into a list of words using the split() method. The program should build a
+list of words. For each word on each line check to see if the word is already
+in the list and if not append it to the list. When the program completes, sort
+and print the resulting words in alphabetical order.
+You can download the sample data at http://www.py4e.com/code3/romeo.txt
+"""
+
+"""
+Starter Code:
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+print(line.rstrip())
+"""
+
+
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+
+new = fh.read().rstrip().split()
+
+for word in new:
+    if word not in lst:
+        lst.append(word)
+
+lst.sort()
+
+print(lst)
